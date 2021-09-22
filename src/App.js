59131fc9f26cbaebdab2f724504509ce501e946b
jhyoon9705 <jhyoon513@naver.com>
2021-09-22 17:24:49 +0900

    function to add tasks

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import { StatusBar, Dimensions } from 'react-native'
+import styled, { ThemeProvider } from 'styled-components/native'
+import { theme } from './theme'
+import Input from './components/Input'
+import Task from './components/Task'
+// import { images } from './images'
+// import IconButton from './components/IconButton'
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+  align-items: center;
+  justify-content: flex-start;
+`
+const Title = styled.Text`
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.main};
+  align-self: flex-start;
+  margin: 0px 20px;
+`
+
+const List = styled.ScrollView`
+  width: ${({ width }) => width - 40}px;
+`
+
+export default function App() {
+  const width = Dimensions.get('window').width
+  const [newTask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState({
+    1: { id: '1', text: 'Hanbit', completed: false },
+    2: { id: '2', text: 'React Native', completed: true },
+    3: { id: '3', text: 'React Native Sample', completed: false },
+    4: { id: '4', text: 'Edit Todo Tiem', completed: false },
+  })
+
+  const _addTask = () => {
+    const ID = Date.now.toString()
+    const newTaskObject = {
+      [ID]: { id: ID, text: newTask, completed: false },
+    }
+    setNewTask('')
+    setTasks({ ...tasks, ...newTaskObject })
+  }
+
+  const _handleTextChange = (text) => {
+    setNewTask(text)
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <StatusBar
+          barstyle="light-content"
+          backgroundColor={theme.background}
+        />
+        <Title>Todo List</Title>
+        <Input
+          placeholder="+ Add a Task"
+          value={newTask}
+          onChangeText={_handleTextChange}
+          onSubmitEditing={_addTask}
+        />
+        <List width={width}>
+          {Object.values(tasks)
+            .reverse()
+            .map((item) => (
+              <Task key={item.id} text={item.text} />
+            ))}
+        </List>
+      </Container>
+    </ThemeProvider>
+  )
+}
---
+++ src/images.js
@@ -0,0 +1,11 @@
+import CheckBox from '../assets/icons/baseline_check_box_black_48.png'
+import CheckBoxOutline from '../assets/icons/baseline_check_box_outline_blank_black_48.png'
+import DeleteForever from '../assets/icons/baseline_delete_forever_white_48.png'
+import Edit from '../assets/icons/baseline_edit_white_48.png'
+
+export const images = {
+  uncompleted: CheckBoxOutline,
+  completed: CheckBox,
+  delete: DeleteForever,
+  update: Edit,
+}
